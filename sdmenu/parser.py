03254679c6187bf388bbbf329af6071b96c4a01a
Gideon Tong <gideon@gideontong.com>
2021-02-17 21:40:53 -0800

    Complete menu cache

--- sdmenu/parser.py
+++ sdmenu/parser.py
@@ -1,6 +1,18 @@
-class page_object:
-    def __init__(self, page):
-        self.data = self.create_page_object(page)
+from requests.models import Response
+from sdmenu.classes import menu_item
+from typing import List
 
-    def create_page_object(self, page):
-        pass
+
+class page_object:
+    def __init__(self, name: str, time_of_day: str, hours: List[List[int]], menu_items: List[menu_item]):
+        self.name = name
+        self.time_of_day = time_of_day
+        self.hours = hours
+        self.menu_items = menu_items
+
+    @classmethod
+    def from_web_call(cls, dining_hall: str, time_of_day: str, page: Response):
+        # TODO: Parse page
+        hours = list()
+        menu_items = list()
+        return cls(dining_hall, time_of_day, hours, menu_items)
