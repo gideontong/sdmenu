2775c6f134f52f01590c16e7f39cc57a6c5bff4c
Gideon Tong <gideon@gideontong.com>
2021-02-17 21:07:50 -0800

    Prepare menu_cache functions appropiate

--- SDHMenu/__init__.py
+++ SDHMenu/__init__.py
@@ -1,10 +1,11 @@
 from SDHMenu.classes import menu_item
+from SDHMenu.webworker import menu_cache
 from typing import List
 
 
 class menu:
     def __init__(self):
-        pass
+        self.cache = menu_cache()
 
     def get(self, restaurant: str) -> List[menu_item]:
         pass
--- SDHMenu/webworker.py
+++ SDHMenu/webworker.py
@@ -1,16 +1,17 @@
 from datetime import datetime, timedelta
+from SDHMenu.parser import page_object
 from typing import Tuple
 
 ALLOWED_DINING_HALLS = {
-    '64 Degrees',
-    'Cafe Ventanas',
-    'Canyon Vista',
-    'Club Med',
-    'Foodworx',
-    'OceanView',
-    'Pines',
-    'Sixth College',
-    'The Bistro'
+    '64 Degrees': '64',
+    'Cafe Ventanas': '18',
+    'Canyon Vista': '24',
+    'Club Med': '15',
+    'Foodworx': '11',
+    'OceanView': '05',
+    'Pines': '01',
+    'Sixth College': '37',
+    'The Bistro': '27'
 }
 
 ALLOWED_TIMES = {
@@ -22,27 +23,38 @@ ALLOWED_TIMES = {
 
 
 def get_current_time() -> Tuple[int, int, int, int, int]:
-    '''The current time of day and date
-
-    Returns: Tuple - Year, month, day, day of week, current hour
-    '''
+    '''The current date and time of day'''
     now = datetime.utcnow() + timedelta(hours=-8)
     return now.year, now.month, now.day, now.weekday(), now.hour
 
+def get_current_time_of_day():
+    year, month, day, weekday, hour = get_current_time()
+    if weekday > 0 or weekday < 6:
+        if hour < 11:
+            return 'Breakfast'
+    else:
+        if hour < 13:
+            return 'Breakfast'
+    if hour < 16:
+        return 'Lunch'
+    else:
+        return 'Dinner'
 
 class menu_cache:
     def __init__(self):
         self.year, self.month, self.day, weekday, hour = get_current_time()
+        self.data = dict()
 
     def needs_update(self) -> bool:
+        '''Check if the combination needs an update'''
         year, month, day, weekday, hour = get_current_time()
-        if self.year != year or self.month != month or self.day != day:
-            pass
-
-
-def get_current_menu_time():
-    pass
-
-
-def get_menu_page(dining_hall: str, time_of_day: str):
-    pass
+        return self.year != year or self.month != month or self.day != day
+    
+    def get_menu_page(dining_hall: str, time_of_day='Auto') -> page_object:
+        '''Returns a menu page for a given dining hall and time of day'''
+        if dining_hall not in ALLOWED_DINING_HALLS:
+            raise KeyError(f'{dining_hall} is not a known dining hall!')
+        if time_of_day not in ALLOWED_TIMES:
+            raise KeyError(f'{time_of_day} invalid, must be one of: Auto, Breakfast, Lunch, Dinner')
+        if time_of_day == 'Auto':
+            time_of_day = get_current_time_of_day()
